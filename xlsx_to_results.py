b9cc9daeb64cd2dfd786a6ffb95f572681c6b58b
Lucy OP13 <lucy@aldatubio.com>
2024-10-17 13:36:37 -0400

    dev: adding tkinter handling

--- xlsx_to_results.py
+++ xlsx_to_results.py
@@ -3,12 +3,16 @@
 ###
 
 import pandas as pd #file and data handling
-from tkinter import Tk, filedialog, messagebox
+from tkinter import filedialog
+import tkinter as tk
 
 
 ###
 ### Read file/sheet as pandas dataframe
 ###
+
+# create root window / main window
+main_window = tk.Tk()
 
 results_file = filedialog.askopenfilename(title = 'Choose results file (.xlsx)', filetypes= [("Excel file","*.xlsx"),("Excel file 97-2003","*.xls")])
 
@@ -16,10 +20,13 @@ results_file = filedialog.askopenfilename(title = 'Choose results file (.xlsx)',
 try:
     results_table = pd.read_excel(results_file, sheet_name = "Results", skiprows = 47)
 except:
-    messagebox.showerror(message='File not selected')
+    tk.messagebox.showerror(message='File not selected. Make sure file is not open in another program.')
     # close program
     raise SystemExit()
 
 print(results_table.loc[:, ["Sample Name", "Target Name", "CT"]])
 results_table["CT"] = results_table["CT"].replace(to_replace = "Undetermined", value = "35").apply(pd.to_numeric)
-print(results_table.loc[:, ["Sample Name", "Target Name", "CT"]])
+print(results_table.loc[:, ["Sample Name", "Comments", "Reporter", "CT"]])
+
+# call main window
+main_window.mainloop()
