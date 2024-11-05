12d206d84aba66e3096d86a60f90201f7a23e360
Lucy OP13 <lucy@aldatubio.com>
2024-11-05 17:16:06 -0500

    dev: added handling for closing main menu window

--- xlsx_to_results.py
+++ xlsx_to_results.py
@@ -27,6 +27,9 @@ root = tk.Tk()
 root.title('Aldatu Biosciences - qPCR Analysis')
 root.geometry('500x350')
 
+# destroy root if close button is clicked
+root.protocol('WM_DELETE_WINDOW', root.destroy)
+
 # center root on user's screen
 def center_window(self):
         self.update_idletasks()
