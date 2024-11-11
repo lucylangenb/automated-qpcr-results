e2e7d5012b86c259b9905f45a824eaec8a297995
Lucy OP13 <lucy@aldatubio.com>
2024-11-11 11:39:40 -0500

    dev: get results data from text file regardless of prior contents

--- txt_to_df.py
+++ txt_to_df.py
@@ -5,6 +5,7 @@
 import pandas as pd #file and data handling
 from tkinter import filedialog
 import tkinter as tk
+import csv
 
 machine_type = 'QuantStudio 5'
 fluor_names = {"CY5": "Internal Control",  
@@ -13,7 +14,7 @@ fluor_names = {"CY5": "Internal Control",
                 }
 cq_cutoff = 35
 
-results_filepath = filedialog.askopenfilename(title = 'Choose results file', filetypes= [("All Excel Files","*.xlsx"),("All Excel Files","*.xls"),("Text Files", "*.txt")])
+results_filepath = filedialog.askopenfilename(title = 'Choose results file', filetypes = [("All Excel Files","*.xlsx"),("All Excel Files","*.xls"),("Text Files", "*.txt")])
 
 print(results_filepath)
 print(f"File extension: {results_filepath.split('.')[1]}")
@@ -22,3 +23,12 @@ if results_filepath.split('.')[1] == 'txt':
     print('Results file is a text file')
 else:
     print('Results file is not a text file')
+
+with open(results_filepath, newline = '') as csvfile:
+    results_reader = csv.reader(csvfile, delimiter = '\t')
+    data_bool = False
+    for line in results_reader:
+        if data_bool == True:
+            print(', '.join(line))
+        if '[Results]' in line:
+            data_bool = True
