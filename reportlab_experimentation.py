766202d90e94ec34b3d8fca46705b02bf3d82286
Lucy OP13 <lucy@aldatubio.com>
2024-12-06 16:36:11 -0500

    feat: experimenting with reportlab, platypus

--- reportlab_experimentation.py
+++ reportlab_experimentation.py
@@ -1,6 +1,48 @@
 from reportlab.pdfgen import canvas
-import sys
 
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.rl_config import defaultPageSize
+from reportlab.lib.units import inch
+
+PAGE_HEIGHT=defaultPageSize[1]; PAGE_WIDTH=defaultPageSize[0]
+styles = getSampleStyleSheet()
+
+Title = "Hello world"
+pageinfo = "platypus example"
+def myFirstPage(canvas, doc):
+    canvas.saveState()
+    canvas.setFont('Times-Bold',16)
+    canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-108, Title)
+    canvas.setFont('Times-Roman',9)
+    canvas.drawString(inch, 0.75 * inch, "First Page / %s" % pageinfo)
+    canvas.restoreState()
+def myLaterPages(canvas, doc):
+    canvas.saveState()
+    canvas.setFont('Times-Roman',9)
+    canvas.drawString(inch, 0.75 * inch, "Page %d %s" % (doc.page, pageinfo))
+    canvas.restoreState()
+
+
+def go():
+    doc = SimpleDocTemplate("phello.pdf")
+    Story = [Spacer(1,2*inch)]
+    style = styles["Normal"]
+    for i in range(100):
+        bogustext = ("This is Paragraph number %s. " % i) *20
+        p = Paragraph(bogustext, style)
+        Story.append(p)
+        Story.append(Spacer(1,0.2*inch))
+    doc.build(Story, onFirstPage=myFirstPage, onLaterPages=myLaterPages)
+
+
+go()
+
+
+
+
+
+'''
 def hello(c):
     c.drawString(100,100,"Hello World")
 c = canvas.Canvas("hello.pdf",
@@ -11,6 +53,14 @@ c = canvas.Canvas("hello.pdf",
                   verbosity=0,
                   encrypt=None
                   )
+
 hello(c)
+#c.addOutlineEntry("Add Outline Entry", "Hello World", level=0, closed=None)
+
+c.setAuthor("Author")
+c.setTitle("Title")
+c.setSubject("Subject")
+
 c.showPage()
 c.save()
+'''
