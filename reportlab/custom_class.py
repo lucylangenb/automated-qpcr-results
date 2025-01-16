63f4884325e0b53f561ea66dbfdcc9e3676ebb09
Lucy OP13 <lucy@aldatubio.com>
2025-01-16 17:02:53 -0500

    feat: custom classes for aldatu header, data tables

--- reportlab/custom_class.py
+++ reportlab/custom_class.py
@@ -5,22 +5,49 @@ import time
 import getpass
 from datetime import datetime
 
-from lxml import objectify
-from reportlab.lib import colors, utils
+import time
 from reportlab.lib.enums import TA_JUSTIFY
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch, mm
+from reportlab.lib import utils, colors
 from reportlab.pdfgen import canvas
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.platypus import Flowable, Indenter, Table, TableStyle
 
 
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.pdfgen import canvas
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Table
-from reportlab.lib.units import inch, mm
+class Header(Flowable):
+    
+    def __init__(self, width=2*inch, height=0.2*inch):
+        Flowable.__init__(self)
+        self.width = width
+        self.height = height
+        self.styles = getSampleStyleSheet()
+
+    def coord(self, x, y, unit=1):
+        x, y = x*unit, self.height - y*unit
+        return x, y
+    
+    def draw(self):
+
+        img_filepath = r"C:\Users\lucy\OneDrive - Aldatu Biosciences\Desktop\PANDAA qPCR Results\vhf\aldatulogo_icon.gif"
+        desired_width = 30
+
+        img = utils.ImageReader(img_filepath) #ImageReader uses Pillow to get information about image, so that we can grab the image size
+        img_width, img_height = img.getSize()
+        aspect = img_height / float(img_width) #calculate aspect ratio based on obtained height and width information
+        img = Image(img_filepath,
+                width=desired_width,
+                height=(desired_width * aspect)) #scale height based on aspect ratio
+        #img.hAlign = 'CENTER'
+        img.wrapOn(self.canv, self.width, self.height)
+        img.drawOn(self.canv, *self.coord(0,0,inch))
+
+        ptext = '<font size=18><b>Report</b></font>'
+        p = Paragraph(ptext, self.styles['Normal'])
+        p.wrapOn(self.canv, self.width, self.height)
+        p.drawOn(self.canv, *self.coord(0.55,-0.2, inch))
+
 
 
 class Report:
@@ -29,7 +56,10 @@ class Report:
     '''
     def __init__(self, pdf_file, pagesize=letter):
         ''''''
-        self.canvas = canvas.Canvas(pdf_file, pagesize=pagesize)
+        self.doc = SimpleDocTemplate(pdf_file, pagesize=pagesize,
+                                     rightMargin=0.75*inch, leftMargin=0.75*inch,
+                                     topMargin=0.75*inch, bottomMargin=0.75*inch)
+        self.elements = []
         self.styles = getSampleStyleSheet()
         self.width, self.height = pagesize
 
@@ -50,66 +80,88 @@ class Report:
         '''.format(size=size, text=text),
            self.styles['Normal'])
 
-    
+   
     def create_header(self):
         ''''''
-        img_filepath = r"C:\Users\lucy\OneDrive - Aldatu Biosciences\Desktop\PANDAA qPCR Results\vhf\aldatulogo_icon.gif"
-        desired_width = 30
-
-        img = utils.ImageReader(img_filepath) #ImageReader uses Pillow to get information about image, so that we can grab the image size
-        img_width, img_height = img.getSize()
-        aspect = img_height / float(img_width) #calculate aspect ratio based on obtained height and width information
-        img = Image(img_filepath,
-                width=desired_width,
-                height=(desired_width * aspect)) #scale height based on aspect ratio
-        #img.hAlign = 'CENTER'
-        img.wrapOn(self.canvas, self.width, self.height)
-        img.drawOn(self.canvas, *self.coord(20,25,mm))
-
-        ptext = '<font size=18><b>Report</b></font>'
-        p = Paragraph(ptext, self.styles['Normal'])
-        p.wrapOn(self.canvas, self.width, self.height)
-        p.drawOn(self.canvas, *self.coord(34, 19.5, mm))
-
+        header = Header()
+        self.elements.append(header)
+    
 
 
     def create_run_info(self):
         ''''''
         ptext = '<font size=14><b>Run Information</b></font>'
         p = Paragraph(ptext, self.styles['Normal'])
-        p.wrapOn(self.canvas, self.width, self.height)
-        p.drawOn(self.canvas, *self.coord(20, 34, mm))
+        self.elements.append(p)
+        self.elements.append(Spacer(1,0.2*inch))
 
-        colWidths = [72, 500]
         filepath = os.path.dirname(os.path.abspath(__file__))
         user = getpass.getuser()
         data = [[self.create_text('Name', bold=True), self.create_text('experiment name')],
-                [self.create_text('File', bold=True), self.create_text(filepath)],
+                [self.create_text('File', bold=True), self.create_text(filepath+'\\'+pdf_file)],
                 [self.create_text('Signature', bold=True), self.create_text('Valid')],
                 [self.create_text('Status', bold=True), self.create_text('Completed')],
                 [self.create_text('Operator', bold=True), self.create_text(user)
                  ]]
+        
+        colWidths = [0.875*inch, 5.95*inch]
+        table_style = TableStyle([('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                                  ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                                  ('VALIGN',(0,0),(-1,-1),'MIDDLE')])
+
         table = Table(data, colWidths=colWidths)
-        table.wrapOn(self.canvas, self.width, self.height)
-        table.drawOn(self.canvas, *self.coord(18, 72, mm)) #coords are for lower left corner of table
+        table.setStyle(table_style)
+        table.hAlign = 'LEFT'
+        self.elements.append(table)
+        self.elements.append(Spacer(1, 0.3*inch))
 
 
     def create_results(self):
         ''''''
-        pass
+        ptext = '<font size=14><b>Samples</b></font>'
+        p = Paragraph(ptext, self.styles['Normal'])
+        self.elements.append(p)
+        self.elements.append(Spacer(1, 0.2*inch))
+
+        well_pos = self.create_text('Well', bold=True)
+        sample = self.create_text('Sample Name', bold=True)
+        result_txt = self.create_text('Result', bold=True)
+        data = [[well_pos, sample, result_txt]]
+
+        result_one = [self.create_text('A1'),
+                      self.create_text('Sample 1'),
+                      self.create_text('LASV Positive')]
+        for item in range(50):
+            data.append(result_one)
+
+        colWidths = [0.5*inch, 3.625*inch, 2.75*inch]
+        table_style = TableStyle([('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                                  ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                                  ('VALIGN',(0,0),(-1,-1),'MIDDLE')])
+
+        table = Table(data,
+                      colWidths=colWidths,
+                      repeatRows=1)
+        table.setStyle(table_style)
+        table.hAlign = 'LEFT'
+        self.elements.append(table)
+
+
+    def create(self):
+        ''''''
+        self.create_header()
+        self.create_run_info()
+        self.create_results()
+        self.save()
+
 
     def save(self):
         ''''''
-        self.canvas.save()
+        self.doc.build(self.elements)
 
 
-def main(pdf_file):
-    ''''''
-    results = Report(pdf_file)
-    results.create_header()
-    results.create_run_info()
-    results.save()
 
 if __name__ == '__main__':
     pdf_file = 'results_example.pdf'
-    main(pdf_file)
+    results = Report(pdf_file)
+    results.create()
