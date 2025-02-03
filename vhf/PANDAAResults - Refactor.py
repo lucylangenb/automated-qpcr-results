e25b75e46afa1331829359fa1ce3661521afc8f3
Lucy OP13 <lucy@aldatubio.com>
2025-02-03 17:31:11 -0500

    refactor: tkinter viz rewritten as class

--- vhf/PANDAAResults - Refactor.py
+++ vhf/PANDAAResults - Refactor.py
@@ -48,135 +48,173 @@ dRn_percent_cutoff = 0.05 #if (sample dRn / max dRn) is less than this value, sa
 ### 0. Main menu
 ##############################################################################################################################
 
-# initialize root window
-root = tk.Tk()
-root.title('Aldatu Biosciences - qPCR Analysis')
-root.geometry('500x350')
+class PandaaMenu:
+    ''''''
+    def __init__(self):
+        ''''''
+        self.root = None
+        self.menu_frame = None
 
-# handle events when close button is clicked
-def close_program():
-    root.destroy() #get rid of root window
-    raise SystemExit() #close program
+        self.window_title = 'Aldatu Biosciences - qPCR Analysis'
+        self.header_title = 'PANDAA qPCR Results Analysis'
+        self.assay_choices = ['PANDAA LASV', 'PANDAA CCHFV', 'PANDAA Ebola + Marburg']
+        self.machine_choices = ['QuantStudio 3', 'QuantStudio 5', 'Rotor-Gene', 'Mic']
 
-root.protocol('WM_DELETE_WINDOW', close_program) #when delete_window event occurs, run close_program function
-
-# center root on user's screen
-def center_window(self):
-        self.update_idletasks()             #check for any changes to window size
-        width = self.winfo_width()          #get root's width and height
-        height = self.winfo_height()
-        x = (self.winfo_screenwidth() // 2) - (width // 2)  #x = half of screen width / half of window width
-        y = (self.winfo_screenheight() // 2) - (height // 2)
-        self.geometry(f'{width}x{height}+{x}+{y}')  #use previously calculated x, y as adjustments to horiz/vert window alignment on screen
-
-center_window(root)
-
-# make sure that, when script is packaged as exe, logo image file can be found
-def get_img_path(filename):
-    if hasattr(sys, '_MEIPASS'):
-        return os.path.join(sys._MEIPASS, filename)
-    else:
-        return filename
-
-# change feather icon in upper corner to Aldatu logo
-ico = Image.open(get_img_path('aldatulogo_icon.gif'))
-ico = ImageTk.PhotoImage(ico)
-root.wm_iconphoto(True, ico) #True bool here ensures that all subsequent windows also use this icon
-
-# resize image
-logo = Image.open(get_img_path('aldatulogo.gif'))
-logo = logo.resize((logo.width//6, logo.height//6)) #pixels are whole numbers, so int division needed
-
-# turn image into tk object
-logo = ImageTk.PhotoImage(logo) #convert logo to tk image object
-logo_label = tk.Label(root,     #tk object logo will live inside
-                      image = logo
-                      ).pack(side = 'top', #where to position, relative to other objects
-                            fill = tk.X,   #if empty space exists in x direction, fill it with logo's background (in this case, transparent)
-                            pady = 15)     #add 15 pixels above/below logo
-
-# window title
-title_label = tk.Label(root,
-                        text = 'PANDAA qPCR Results Analysis',
-                        font = ('Arial', 12, 'bold')
-                        ).pack(side = 'top',
-                                fill = tk.X,
-                                pady = 10)
+        self.assay = None
+        self.machine = None
 
 
-# frame to hold questions
-questions_frame = tk.Frame(root)
-questions_frame.pack(side = 'top')
+    def center_window(self):
+        '''Center window on user's screen.'''
+        self.root.update_idletasks()             #check for any changes to window size
+        width = self.root.winfo_width()          #get root's width and height
+        height = self.root.winfo_height()
+        x = (self.root.winfo_screenwidth() // 2) - (width // 2)  #x = half of screen width / half of window width
+        y = (self.root.winfo_screenheight() // 2) - (height // 2)
+        self.root.geometry(f'{width}x{height}+{x}+{y}')  #use previously calculated x, y as adjustments to horiz/vert window alignment on screen
 
-# frame to hold information about assay type choice
-assay_type_frame = tk.Frame(questions_frame)
-assay_type_frame.pack(side = 'left', padx = 20)
 
-# question title - assay choice
-assay_type_label = tk.Label(assay_type_frame,
+    def close_program(self):
+        '''Handle events when close button is clicked on tkinter window.'''
+        try:
+            self.root.destroy() #get rid of root window
+        except Exception as e:
+            print(f"Error while closing resources: {e}")
+        finally:
+            raise SystemExit() #close program
+
+
+    def get_image(self, filename:str, resize=None, aspect=None):
+        '''Image processor: make sure that, when script is packaged as exe, logo image file can be found.'''
+        path = os.path.join(sys._MEIPASS, filename) if hasattr(sys, '_MEIPASS') else filename
+        img = Image.open(path)
+        if resize:
+            if aspect:
+                img = img.resize((round(img.width*aspect),
+                                  round(img.height*aspect)))
+            else:
+                img = img.resize(resize)
+        return ImageTk.PhotoImage(img)
+
+
+    def init_root(self):
+        '''Initialize root tkinter window.'''
+
+        self.root = tk.Tk()
+        self.root.title(self.window_title)
+        self.root.geometry('500x350')
+
+        # when delete_window event occurs, run close_program function
+        self.root.protocol('WM_DELETE_WINDOW', self.close_program)
+        # center root on user's screen
+        self.center_window()
+
+        # change feather icon in upper corner to Aldatu logo
+        ico = self.get_image('aldatulogo_icon.gif')
+        self.root.wm_iconphoto(True, ico) #True bool here ensures that all subsequent windows also use this icon
+
+
+    def add_header(self):
+        '''Add header image and text to tkinter window.'''
+        self.logo = self.get_image('aldatulogo.gif', resize=True, aspect=1/6)
+        # tk object logo will live inside logo_label
+        logo_label = tk.Label(self.root, image = self.logo
+                             ).pack(side = 'top', #where to position, relative to other objects
+                                    fill = tk.X,   #if empty space exists in x direction, fill it with logo's background (in this case, transparent)
+                                    pady = 15)     #add 15 pixels above/below logo
+        # window title
+        title_label = tk.Label(self.root, text = self.header_title,
+                               font = ('Arial', 12, 'bold')
+                              ).pack(side = 'top',
+                                     fill = tk.X,
+                                     pady = 10)
+        # frame to hold questions
+        self.menu_frame = tk.Frame(self.root)
+        self.menu_frame.pack(side='top')
+    
+
+    def add_assay_choice(self):
+        '''Add menu for the user to select an assay.'''
+        # frame to hold information about assay type choice
+        assay_frame = tk.Frame(self.menu_frame)
+        assay_frame.pack(side='left', padx=20)
+
+        # question title - assay choice
+        assay_label = tk.Label(assay_frame,
                                 text = 'Choose assay results to analyze:',
                                 font = ('Arial', 10)
                                 ).pack(side = 'top',
                                     fill = tk.X,
                                     pady = 2)
+        
+        # radio buttons - assay choice
+        self.assay_var = tk.StringVar()
+        self.assay_var.set(None) #initialize - forces radio buttons to be empty upon loading screen
 
-# radio buttons - assay choice
-assays = ['PANDAA LASV',
-        'PANDAA CCHFV',
-        'PANDAA Ebola + Marburg']
-assay_var = tk.StringVar()
-assay_var.set(None) #initialize - forces radio buttons to be empty upon loading screen
+        for option in self.assay_choices:
+            tk.Radiobutton(assay_frame,
+                            text = option,
+                            padx = 20,
+                            variable = self.assay_var,
+                            command = self.assay_var.get(),
+                            value = option
+                            ).pack(anchor = tk.W) #center text vertically around reference point, left-justified - "W" refers to "West"
+            
+    
+    def add_machine_choice(self):
+        '''Add menu for the user to select a qPCR machine.'''
+        machine_frame = tk.Frame(self.menu_frame)
+        machine_frame.pack(side = 'right', padx = 20)
 
-for option in assays:
-    tk.Radiobutton(assay_type_frame,
-                    text = option,
-                    padx = 20,
-                    variable = assay_var,
-                    command = assay_var.get(),
-                    value = option
-                    ).pack(anchor = tk.W) #center text vertically around reference point, left-justified - "W" refers to "West"
+        # question title for instrument choice
+        machine_label = tk.Label(machine_frame,
+                                    text = 'qPCR machine used:',
+                                    font = ('Arial', 10)
+                                    ).pack(side = 'top',
+                                            fill = tk.X,
+                                            pady = 2)
 
-# instrument choice
-machine_type_frame = tk.Frame(questions_frame)
-machine_type_frame.pack(side = 'right', padx = 20)
-
-# question title for instrument choice
-machine_label = tk.Label(machine_type_frame,
-                            text = 'qPCR machine used:',
-                            font = ('Arial', 10)
-                            ).pack(side = 'top',
-                                    fill = tk.X,
-                                    pady = 2)
-
-# drop-down for instrument choice
-machines = ['QuantStudio 3',
-                'QuantStudio 5',
-                'Rotor-Gene',
-                'Mic']
-machine_var = tk.StringVar(value = 'QuantStudio 5') # includes initialization
-
-machine_menu = tk.OptionMenu(machine_type_frame,
-                            machine_var, #initial value in drop-down
-                            *machines,   #values to choose from
-                            command = lambda x: machine_var.get()
-                            ).pack(anchor = tk.W)
+        # drop-down for instrument choice
+        self.machine_var = tk.StringVar(value = 'QuantStudio 5') # includes initialization
+        machine_menu = tk.OptionMenu(machine_frame,
+                                    self.machine_var, #initial value in drop-down
+                                    *self.machine_choices,   #values to choose from
+                                    command = lambda x: self.machine_var.get()
+                                    ).pack(anchor = tk.W)
 
 
-# button to continue to file selection after selections have been made
-def ok_click():
-    global assay, machine_type #to edit global variables within a function, need to specify that we're editing the global verisons of these variables
-    assay = assay_var.get()
-    machine_type = machine_var.get()
-    root.destroy()
+    def ok_click(self):
+        self.assay = self.assay_var.get()
+        self.machine = self.machine_var.get()
+        self.close_program()
 
-ok_button = tk.Button(root,
-                    text = 'Select results file...',
-                    command = ok_click)
-ok_button.pack(side = 'bottom',
-               pady = 30)
 
-root.mainloop()
+    def add_filebutton(self):
+        '''Add button for user to proceed to file selection.'''
+        ok_button = tk.Button(self.root,
+                              text = 'Select results file...',
+                              command = self.ok_click)
+        ok_button.pack(side = 'bottom', pady = 30)
+            
+    
+    def start(self):
+        '''Run PandaaMenu.'''
+        self.init_root()
+        self.add_header()
+        self.add_assay_choice()
+        self.add_machine_choice()
+        self.add_filebutton()
+        self.root.mainloop()
+        print('Running')
 
+
+if __name__ == '__main__':
+    app = PandaaMenu()
+    app.start()
+    print('{0} {1}'.format(app.assay, app.machine))
+
+
+'''
 
 ##############################################################################################################################
 ### 1. Initialization
@@ -292,3 +330,4 @@ while not file_saved:
 
 tk.messagebox.showinfo(title="Success", message=f"Summary results saved in:\n\n{summary_filepath}")
 root.destroy()
+'''
